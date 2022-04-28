301af8fe8366e9153c0a06048b3ce4f439f7c2a3
Krzysztof Rykowski <rykowski.krzysztof@gmail.com>
2022-04-28 15:41:09 +0200

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CV_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Contacts()
+        {
+            return View();
+        }
+
+        public IActionResult Skills()
+        {
+            return View();
+        }
+
+        public IActionResult Education()
+        {
+            return View();
+        }
+
+        public IActionResult Experience()
+        {
+            return View();
+        }
+
+        public IActionResult Languages()
+        {
+            return View();
+        }
+        
+        public IActionResult Hobby()
+        {
+            return View();
+        }
+    }
+}
---
+++ wwwroot/js/site.js
@@ -0,0 +1,21 @@
+﻿const section = document.querySelector('section');
+const arrowDiv = document.querySelector('.arrow');
+const arrowImg = document.querySelector('.arrow img');
+
+window.addEventListener('scroll', function () {
+    const scrollValue = this.scrollY;
+    const viewportHeight = window.innerHeight;
+
+    if (scrollValue > 0) {
+        arrowDiv.classList.add('active');
+        arrowImg.classList.add('roll');
+    }
+    else {
+        arrowDiv.classList.remove('active');
+        arrowImg.classList.remove('roll');
+    }
+})
+
+window.addEventListener('load', function () {
+    section.classList.add('visible');
+})
